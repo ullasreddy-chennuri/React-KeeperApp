d54bcc79025257b3b89cfad93e303b70ef6a6c86
ullasreddy-chennuri <ullasreddy3663@gmail.com>
2020-08-24 19:47:54 +0530

    Created Default Notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>Title of Note</h1>
-        <p>Paragraph in Note</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 }
 
